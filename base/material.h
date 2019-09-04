497ae443b0603f4670e04d431834710dd9380563
yrq110 <yrq1025@gmail.com>
2019-09-04 10:06:11 +0800

    feat: add chapter-19 volumes

--- base/material.h
+++ base/material.h
@@ -132,4 +132,15 @@ class diffuse_light : public material  {
         texture *emit;
 };
 
+class isotropic : public material {
+    public:
+        isotropic(texture *a) : albedo(a) {}
+        virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const  {
+             scattered = ray(rec.p, random_in_unit_sphere(), r_in.time());
+             attenuation = albedo->value(rec.u, rec.v, rec.p);
+             return true;
+        }
+        texture *albedo;
+};
+
 #endif
